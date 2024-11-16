f5a6c3bdd6024aa19adffc73313847237d703022
Alex Folombas <folombas@yandex.ru>
2024-11-16 15:13:41 +0300

    removeListener

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-// Events Emitter
-	const EventEmitter = require('events');
+const EventEmitter = require('events'); // Events Emitter
 
+const myEmitter = new EventEmitter();
+
+const logDbConnection = () => {
+	console.log('DB Connected');
+};
+
+myEmitter.addListener('connection', logDbConnection);
+myEmitter.emit('connection');
+
+
+myEmitter.removeListener('connection', logDbConnection);
+// myEmitter.off('connection', logDbConnection);
+// myEmitter.removeAllListeners('connected')
+myEmitter.emit('connected');
+
+myEmitter.on('msg', (data) => {
+	console.log(`Получил: ${data}`);
+});
+myEmitter.emit('msg', 'Привет! Получи моё сообщение!');
