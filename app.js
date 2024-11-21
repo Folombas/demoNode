688a473fba44bce1770d60a22825629b4b39f46b
Alex Folombas <folombas@yandex.ru>
2024-11-22 00:43:35 +0300

    Parsing package.json

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-let outter = null; // Работа с памятью
-let run = function () {
-	let inner = outter
-	let unused = function () {
-		if (inner)
-			console.log("hi")
-	}
-	outter = {
-		longStr: new Array(1000000).join('*'),
-	};
-};
-setInterval(run, 1000);
+//Разбор package.json
