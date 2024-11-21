5fd94fa78c9071e19e49666dc8bdcdc441aea13c
Alex Folombas <folombas@yandex.ru>
2024-11-21 22:13:16 +0300

    Fork vs Worker

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
-const { spawn } = require('child_process'); //spawn ana exec
+// Упражнение - производительность потоков, Fork vs Worker
+const { Worker } = require('worker_threads');
+const { performance, PerformanceObserver} = require('perf_hooks');
+const { fork } = require('child_process');
+const { readFileSync } = require('fs');
 
-const childProcess = spawn('ls') ;
+const file = readFileSync('./file.mp4');
 
-childProcess.stdout.on('data', (data) => {
-	console.log(`Stdout: ${data}`)
-})
+const performanceObserver = new PerformanceObserver((items) => {
+	items.getEntries().forEach((entry) => {
+		console.log(`${entry.name}: ${entry.duration}`);
+	});
+});
+performanceObserver.observe({ entryTypes: ['measure'] });
 
-childProcess.stderr.on('data', (data) => {
-	console.log(`Stderror: ${data}`)
-})
+const workerFunction = (array) => {
+	return new Promise((resolve, reject) => {
+		performance.mark('worker start');
+		const worker = new Worker('./worker.js', {
+			workerData: { array, file }
+	});
+	worker.on('message', (msg) => {
+		performance.mark('worker end');
+		performance.measure('worker', 'worker start', 'worker end');
+		resolve(msg);
+		});
+	});
+}
 
-childProcess.on('exit', (code) => {
-	console.log(`Код выхода: ${code}`)
-})
+
+const forkFunction = array => {
+	return new Promise((resolve, reject) => {
+		performance.mark('fork start');
+		const forkProcess = fork('./fork.js');
+		forkProcess.send({ array, file });
+		forkProcess.on('message', (msg) => {
+			performance.mark('fork end');
+			performance.measure('fork', 'fork start', 'fork end');
+			resolve(msg);
+		});
+	});
+}
+
+const main = async () => {
+	await workerFunction([25, 19, 48, 30]);
+	await forkFunction([25, 19, 48, 30]);
+}
+
+main();
