f8ed1532de8eea4a380dd310fbcf6d6c238691f1
Alex Folombas <folombas@yandex.ru>
2024-11-23 00:53:05 +0300

    Working with the file system

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,16 +1,35 @@
 import { homedir } from 'os';
 import { join } from 'path';
+import { promises } from 'fs';
 
 const filePath = join(homedir(), 'weather-data.json');
 
-const saveKeyValue = (key, value) => {
-	console.log(basename(filePath));
-	console.log(dirname(filePath));
-	console.log(relative(filePath, dirname(filePath)));
-	console.log(relative(filePath, dirname(filePath)));
-	console.log(isAbsolute(filePath));
-	console.log(resolve('..'));
-	console.log(sep);
+const saveKeyValue = async (key, value) => {
+	let data = {};
+	if (await isExists(filePath)) {
+		const file = await promises.readFile(filePath);
+		data = JSON.parse(file);
+	}
+	data[key] = value;
+	await promises.writeFile(filePath, JSON.stringify(data));
 };
 
-export { saveKeyValue };
+const getKeyValue = async (key) => {
+	if (await isExists(filePath)) {
+		const file = await promises.readFile(filePath);
+		const data = JSON.parse(file);
+		return data[key];
+	}
+	return undefined;
+};
+
+const isExists = async (path) => {
+	try {
+		await promises.stat(path);
+		return true;
+	} catch(e) {
+		return false;
+	}
+};
+
+export { saveKeyValue, getKeyValue };
